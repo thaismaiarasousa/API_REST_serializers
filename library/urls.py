f88185391b0c173409362c64b252c9cf3e5d2eaf
thaismaiarasousa <thais-mns@hotmail.com>
2023-07-15 19:48:25 -0300

    Adicionando Swagger

--- library/urls.py
+++ library/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# pylint: disable=E0611
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from rest_framework import permissions
+# pylint: disable=E0401
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
 
 from rest_framework import routers
 
@@ -28,7 +34,27 @@ route = routers.DefaultRouter()
 # pylint: disable=E1101
 route.register(r'books/', booksviewsets.BooksViewSet, basename="Books")
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include(route.urls))
 ]
+
+# concateno as duplas urlpatterns (a minha e a do swagger) com +=
+urlpatterns += [
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] 
